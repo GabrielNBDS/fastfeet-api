ad28fa872ec1412e1d132eb70d4caca9be559a29
GabrielNBDS <gabriel.nbds@gmail.com>
2022-02-08 17:21:29 -0400

    👌 IMPROVE: Using PG instead of sqlite

--- env.ts
+++ env.ts
@@ -21,4 +21,9 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+  DB_CONNECTION: Env.schema.string(),
+  PG_HOST: Env.schema.string(),
+  PG_PORT: Env.schema.string(),
+  PG_USER: Env.schema.string(),
+  PG_DB_NAME: Env.schema.string(),
 })
