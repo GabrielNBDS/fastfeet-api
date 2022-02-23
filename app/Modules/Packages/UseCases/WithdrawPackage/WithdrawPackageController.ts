220e3df54189124cb1d8d668d1cfcebe3026b4e7
GabrielNBDS <gabriel.nbds@gmail.com>
2022-02-22 22:37:07 -0400

    👌 IMPROVE: Cant withdraw an withdrawn package

--- app/Modules/Packages/UseCases/WithdrawPackage/WithdrawPackageController.ts
+++ app/Modules/Packages/UseCases/WithdrawPackage/WithdrawPackageController.ts
@@ -2,10 +2,20 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Package from '../../Models/Package'
 import WithdrawPackageService from './WithdrawPackageService'
 
-export default async function WithdrawPackageController({ params, bouncer }: HttpContextContract) {
+export default async function WithdrawPackageController({
+  params,
+  bouncer,
+  response,
+}: HttpContextContract) {
   const packageToBeWithdrawn = await Package.findOrFail(params.id)
 
   await bouncer.with('PackagePolicy').authorize('isOwner', packageToBeWithdrawn)
+
+  if (!packageToBeWithdrawn.withdrawalDate) {
+    return response.badRequest({
+      message: 'Pacote já foi retirado',
+    })
+  }
 
   return await WithdrawPackageService(packageToBeWithdrawn)
 }
